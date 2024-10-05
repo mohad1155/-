f6235e3c08ad3353ad56428fa93fc9e399e2081a
mohad1155 <mohamed00976al@gmail.com>
2024-10-05 19:28:42 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-const express = require("express")
-const app = express();
-var listener = app.listen(process.env.PORT || 2000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
-app.listen(() => console.log("I'm Ready To Work..! 24H"));
-app.get('/', (req, res) => {
-  res.send(`
-  <body>
-  <center><h1>Bot 24H ON!</h1></center
-  </body>`)
-});require('dotenv').config();  // تحميل المتغيرات من ملف .env
+require('dotenv').config();  // تحميل المتغيرات من ملف .env
 
 const { Client } = require('discord.js-selfbot-v13');
 const { joinVoiceChannel } = require('@discordjs/voice');
